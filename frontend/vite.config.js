748b7cd73236d8c18938671915ea160fcf028a80
Ritik Panta <officialnpr10@gmail.com>
2026-04-09 12:52:56 +1000

    Changed vite and added vercel to stop the refresh crashout error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true, // ← add this
+    proxy: {},  // only if you have a proxy setup
+  },
+  preview: {
+    port: 5173,
   }
 })
